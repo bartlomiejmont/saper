2de3547aa8a15fca6bb96337d77d59239464b6ec
Bartlomiej Mont <bartlomiejmont@gmail.com>
2020-01-09 15:19:34 +0100

    add map & click on bomb

--- src/GUI/Tile.java
+++ src/GUI/Tile.java
@@ -9,12 +9,12 @@ import javafx.scene.text.Text;
 
 public class Tile extends StackPane {
 
-    private int x, y;
-    private boolean hasBomb;
-    private boolean isOpen = false;
+    public int x, y;
+    public boolean hasBomb;
+    public boolean isOpen = false;
 
     private Rectangle border = new Rectangle(MainLogic.getInstance().TILE_SIZE - 2, MainLogic.getInstance().TILE_SIZE - 2);
-    private Text text = new Text();
+    public Text text = new Text();
 
     public Tile(){ }
 
@@ -26,13 +26,29 @@ public class Tile extends StackPane {
         border.setStroke(Color.LIGHTGRAY);
 
         text.setFont(Font.font(18));
-        text.setText(hasBomb ? "X" : "Y");
-        text.setVisible(true);
+        text.setText(hasBomb ? "X" : "");
+        text.setFill(Color.WHITE);
+        text.setVisible(false);
 
         getChildren().addAll(border, text);
 
         setTranslateX(x * MainLogic.getInstance().TILE_SIZE);
         setTranslateY(y * MainLogic.getInstance().TILE_SIZE);
+
+        setOnMouseClicked(e -> open());
+    }
+
+    public void open() {
+        if (isOpen)
+            return;
+
+        isOpen = true;
+        text.setVisible(true);
+        MainLogic.getInstance().moves.push(this);
+
+//        if (text.getText().isEmpty()) {
+//            MainLogic.getInstance().getNeighbors(this).forEach(Tile::open);
+//        }
     }
 
 }
--- src/LOGIC/MainLogic.java
+++ src/LOGIC/MainLogic.java
@@ -4,6 +4,10 @@ import GUI.Tile;
 import javafx.scene.Group;
 import javafx.stage.Window;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 
 public final class MainLogic {
 
@@ -25,6 +29,8 @@ public final class MainLogic {
     public final int TILE_SIZE = 80;
     public final int W = 800;
     public final int H = 600;
+    public Stack moves = new Stack();
+    public Stack undoMoves = new Stack();
 
     public int X_TILES = 7;
     public int Y_TILES = 7;
@@ -50,7 +56,53 @@ public final class MainLogic {
                 MainLogic.getInstance().grid[x][y] = tile;
             }
         }
+
+        for (int y = 0; y < MainLogic.getInstance().Y_TILES; y++) {
+            for (int x = 0; x < MainLogic.getInstance().X_TILES; x++) {
+                Tile tile = MainLogic.getInstance().grid[x][y];
+
+                if(MainLogic.getInstance().map[x][y]>0){
+                    tile.text.setText(String.valueOf(MainLogic.getInstance().map[x][y]));
+                    tile.text.setVisible(true);
+                }
+            }
+        }
+
         return group;
+    }
+
+    public List<Tile> getNeighbors(Tile tile) {
+        List<Tile> neighbors = new ArrayList<>();
+
+        // ttt
+        // tXt
+        // ttt
+
+        int[] points = new int[] {
+                -1, -1,
+                -1, 0,
+                -1, 1,
+                0, -1,
+                0, 1,
+                1, -1,
+                1, 0,
+                1, 1
+        };
+
+        for (int i = 0; i < points.length; i++) {
+            int dx = points[i];
+            int dy = points[++i];
+
+            int newX = tile.x + dx;
+            int newY = tile.y + dy;
+
+            if (newX >= 0 && newX < X_TILES
+                    && newY >= 0 && newY < Y_TILES) {
+                neighbors.add(grid[newX][newY]);
+            }
+        }
+
+        return neighbors;
     }
 
 
